694467e5c181da7f07b26d112a217be3875e3686
kenneth898 <kennethtan0601@gmail.com>
2024-08-15 16:40:40 +0800

    content

--- src/components/Menuu.vue
+++ src/components/Menuu.vue
@@ -1,4 +1,101 @@
 <template>
+	<h3>
+		{{ $t('content1.1') }}|
+		{{ $t('content1.2') }}|
+		{{ $t('content1.3') }}
+	</h3>
+	<p>
+		{{ $t('text.1') }}
+		<a style="font-weight: 900; color:#800000;" href="https://www.dior88.info/">
+			{{ $t('text.2') }}</a>
+		{{ $t('text.3') }}
+		<b>{{ $t('text.4') }}</b>
+		{{ $t('text.5') }}
+	</p>
+	<br>
+	<p>
+		{{ $t('text.6') }}
+		<b>{{ $t('text.7') }}</b>
+		{{ $t('text.8') }}
+	</p>
+	<br>
+	<h3>
+		{{ $t('wheretostart.title') }}
+	</h3>
+	<p>
+		{{ $t('wheretostart.1') }}
+		<b> {{ $t('wheretostart.2') }}</b>
+		{{ $t('wheretostart.3') }}
+	</p>
+	<br>
+	<ol>
+		<li>
+			{{ $t('wheretostart.4') }}
+			<b>{{ $t('wheretostart.5') }}</b>
+			{{ $t('wheretostart.6') }}
+		</li>
+		<li>
+			{{ $t('wheretostart.7') }}
+		</li>
+		<li>
+			{{ $t('wheretostart.8') }}
+			<b>{{ $t('wheretostart.9') }}</b>
+			{{ $t('wheretostart.10') }}
+		</li>
+	</ol>
+	<br>
+	<p>{{ $t('wheretostart.11') }}</p>
+	<br>
+	<h3>
+		{{ $t('ewalletcasionotransfer.title') }}
+	</h3>
+	<p>
+		{{ $t('ewalletcasionotransfer.1') }}
+		<b>{{ $t('ewalletcasionotransfer.2') }}</b>
+		{{ $t('ewalletcasionotransfer.3') }}
+		<b>{{ $t('ewalletcasionotransfer.4') }}</b>
+		{{ $t('ewalletcasionotransfer.5') }}
+	</p>
+	<br>
+	<h3>
+		{{ $t('onlinemalysia.title') }}
+	</h3>
+	<p>
+		{{ $t('onlinemalysia.1') }}
+		<b>{{ $t('onlinemalysia.2') }}</b>
+		{{ $t('onlinemalysia.3') }}
+	</p>
+	<ul>
+		<li>
+			<b>{{ $t('onlinemalysia.4') }}</b>
+			{{ $t('onlinemalysia.5') }}
+		</li>
+		<li>
+			<b>{{ $t('onlinemalysia.6') }}</b>
+			{{ $t('onlinemalysia.7') }}
+		</li>
+		<li>
+			<b>{{ $t('onlinemalysia.8') }}</b>
+			{{ $t('onlinemalysia.9') }}
+		</li>
+	</ul>
+	<br>
+	<p>
+		{{ $t('onlinemalysia.10') }}
+		<b>{{ $t('onlinemalysia.11') }}</b>
+		{{ $t('onlinemalysia.12') }}
+	</p>
+	<br>
+	<h3>
+		{{ $t('casinoapp.title') }}
+	</h3>
+	<p>
+		{{ $t('casinoapp.1') }}
+		<b>{{ $t('casinoapp.2') }}</b>
+		{{ $t('casinoapp.3') }}
+	</p>
+
+
 	<a href="//www.dmca.com/Protection/Status.aspx?ID=bd071465-17bf-4b90-b6e5-d51ffeb2271e"
 		title="DMCA.com Protection Status" class="dmca-badge"> <img
 			src="https://images.dmca.com/Badges/dmca_copyright_protected150c.png?ID=bd071465-17bf-4b90-b6e5-d51ffeb2271e"
@@ -10,17 +107,24 @@
 	</p>
 
 	<p>
-		<a style="font-weight: 900; color:#800000;" href="https://www.atascasinobet.com/">{{
-			$t('Footer.1') }}</a>
-		| {{ $t('Footer.2') }}|
+		{{ $t('Footer.1') }}
+		| <a style="font-weight: 900; color:#800000;" href="https://www.ataskasino.com/">{{ $t('Footer.2') }}</a> |
 		{{ $t('Footer.3') }}
+		<b>{{ $t('Footer.4') }}</b>
+		{{ $t('Footer.5') }}
+	</p>
+	<br>
+	<p>
+		{{ $t('Footer.6') }}|
+		{{ $t('Footer.7') }}|
+		{{ $t('Footer.8') }}
+		<a style="font-weight: 900; color:#800000;" href="https://www.atascasinobet.com/">{{ $t('Footer.9') }}</a>
+		{{ $t('Footer.10') }}
+		<b>{{ $t('Footer.11') }}</b>
+		{{ $t('Footer.12') }}
 	</p>
 
-	<p style="padding-bottom: 80px;">
-		<a style="font-weight: 900;color:#800000; " href="https://www.ataskasino.com/">{{ $t('Footer.4')
-			}}</a>
-		| {{ $t('Footer.5') }}
-	</p>
+
 
 </template>
 <script>
